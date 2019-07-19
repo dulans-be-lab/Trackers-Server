aca344adb7475d698700d45c6444ccf79270bec6
dulanwirajith <dulanwirajith1995@gmail.com>
2019-07-19 11:02:06 +0530

    create app.js and server is running now

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+  // next eken thamai eelanga middleware ekata call karanne
+  res.send('from express server');
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const http = require('http');
+const app = require('./app');
 
+const port = process.env.PORT || 3000;
+// const server = http.createServer((req, res) => {
+//   res.end('This is me');
+// });
 
-const server = http.createServer((req, res) => {
-  res.end('This is me');
-});
-
-server.listen(process.env.PORT || 3000);
+// request and response serama handle wenne express haraha nisa server eka hadanawa express framework eka yodaagaththa app kiyana variable eken
+const server = http.createServer(app);
+app.set('port', port);
+server.listen(port);
