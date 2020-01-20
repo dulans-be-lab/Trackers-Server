49e1cf84d13af4a7f442c930f277a73ebd59a2d4
dulanwirajith <dulanwirajith1995@gmail.com>
2020-01-20 08:54:11 +0530

    Review K

--- api/v1.0.0/services/mobile-applications/passenger-app/entity/Reviews_Entity.js
+++ api/v1.0.0/services/mobile-applications/passenger-app/entity/Reviews_Entity.js
@@ -7,17 +7,34 @@ const Reviews_Entity = mongoose.Schema({
         unique: true,
         match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
     },
+    reviewed_date: {
+        type: Date,
+
+        required: true
+
+    },
+    bus_number: {
+        type: String,
+        unique: true,
+        required: true
+
+    },
     driver_discipline: {
         type: Number,
-        default: 100
+        default: 5
     },
     driver_arrival_time: {
         type: Number,
-        default: 100
+        default: 5
     },
     bus_condition: {                    // keep it separate or add to the overall score
-        type: Number
+        type: Number,
+        default: 5
+    },
+    passenger_comment:{
+        type:String
     }
+
 });
 
 module.exports = mongoose.model('reviews', Reviews_Entity);
